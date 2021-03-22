f2e8e856a44a2d877104d91d503d0075779c96c2
Jimmie Elvenmark <flugsio@gmail.com>
2021-03-22 10:35:48 +0100

    first test

---
+++ What type of instructional designer are you - Storyline output/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Lzrcq5XM85":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  alert(player.GetVar("4_Avoiding"));
+}
+
